fd4a325c1d8ba9b99d6679745e1e9de1ad57c685
KotaroFujikawa <kotaro28.ktai@gmail.com>
2016-11-17 18:22:10 +0900

    Add files via upload

--- calculator.php
+++ calculator.php
@@ -1,26 +1,38 @@
-
 <html>
 <head>
-
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <title>電卓</title>
 </head>
 <body>
 
 <?php
-$number1=$_POST["post_data1"];
-$number2=$_POST["post_data2"];
+if (isset($_POST["post_data1"])) {
+    $number1=$_POST["post_data1"];
+} else {
+    $number1='';
+}
+if (isset($_POST["post_data2"])) {
+    $number2=$_POST["post_data2"];
+} else {
+    $number2='';
+}
+if (isset($_POST["symbol"])) {
+    $symbol=$_POST["symbol"];
+} else {
+    $symbol='';
+}
+
 $answer = $number1 - $number2;
-} elseif ($symbol == '*') {
-$symbol=$_POST["symbol"];
 
 if ($symbol == '+') {
     $answer = $number1 + $number2;
 } elseif ($symbol == '-') {
-$answer = $number1 * $number2;
+$answer = $number1 - $number2;
 } elseif ($symbol == '/') {
-$answer = $number1 / $number2;
-}
+    $answer = $number1 / $number2;
+}elseif ($symbol == '*') {
+        $answer = $number1 * $number2;
+    }
 ?>
 
 <form action="calculator.php" method="post">
